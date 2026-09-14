8d8afc49c0dacc05006a30be9edc68b1043f389a
Zaid Rasool <zaidrasool56@gmail.com>
2026-09-14 17:17:44 +0500

    comments

--- print-vars/first-print.go
+++ print-vars/first-print.go
@@ -1,10 +1,15 @@
 package main
 import "fmt"
-
+// this is a simple program that demonstrates the use of variables in Go.
 func main(){
 	age := 10
 	height := 20
 	sayHello := "Hello"
+	/* Another way to declare variables in Go is to use the var keyword
+	var age int = 10
+	var height int = 20
+	var sayHello string = "Hello"	
+	*/
 	fmt.Println("slalikum ustaad")
 	fmt.Println("Age:", age)
 	fmt.Println("Height:", height)
