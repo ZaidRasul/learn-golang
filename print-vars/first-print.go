bfc21463359f3bbb38524b2dba98be2cb50843a3
Zaid Rasool <zaidrasool56@gmail.com>
2026-09-14 17:13:57 +0500

    change structure again and vars

--- print-vars/first-print.go
+++ print-vars/first-print.go
@@ -2,6 +2,12 @@ package main
 import "fmt"
 
 func main(){
+	age := 10
+	height := 20
+	sayHello := "Hello"
 	fmt.Println("slalikum ustaad")
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Say Hello:", sayHello)
 }
 
